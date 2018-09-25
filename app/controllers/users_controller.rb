0f6d10a120adbe55088f207b2ddced841e63b78c
klobukov <32955270+klobukov@users.noreply.github.com>
2018-09-25 12:10:03 +0300

    chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]) # найти в бд юзера с id = 1
   end
+
+  def create
+    @user = User.new(user_params)    # Не окончательная реализация!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!" # добавить к flash сообщениям . вывод их в application.html.erb
+      redirect_to @user # перенаправит на /users/:id
+    else
+      render 'new'
+    end
+  end
+
+  private 
+
+  def user_params
+    # параметр user делаем обязательным, а остальные - разрешаем
+    # делается для того, чтоб с помощью например curl
+    # нельзя было передать левые параметры, которые бы попали в бд
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
